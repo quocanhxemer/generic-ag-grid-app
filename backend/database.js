23e055566f89cf804d28cce97fd3c260db3fb5f1
Anh Pham <quocanhxemer@gmail.com>
2025-11-18 02:15:14 +0100

    Typo

--- backend/database.js
+++ backend/database.js
@@ -10,7 +10,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.MYSWL_HOST,
+  host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
--- backend/default-database/index.js
+++ backend/default-database/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 let pool;
 try {
   pool = mysql.createPool({
-    host: process.env.MYSWL_HOST,
+    host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
